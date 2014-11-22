d39d2807abd84b1e364b4fa984f982ce32a2fa50
Byran Wills-Heath <byran@adgico.co.uk>
2014-11-22 11:23:00 +0000

    Added script to update the server from the current branch

---
+++ admin_scripts/update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Use to update the cyberdojo git repo on the cyber-dojo server
+
+# get latest source from https://github.com/JonJagger/cyber-dojo
+# if it asks for a password just hit return
+cd /var/www/cyber-dojo
+git pull
+ret=$?
+if [ $ret -ne 0 ]; then
+  exit
+fi
+
+# restart apache
+service apache2 restart
